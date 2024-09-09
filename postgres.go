7ac94f833e94fe4e249f100a7a91a10bc79cbfc7
Abhishek <abhi@abhishekanand.info>
2024-09-09 14:59:38 +0530

    Test: Custom marshaling and unmarshaling for @id attribute in routes

--- postgres.go
+++ postgres.go
@@ -335,7 +335,7 @@ func getConfiguration() ([]byte, error) {
                 if err != nil {
                     return nil, fmt.Errorf("error marshaling updated http app config: %w", err)
                 }
-                caddy.Log().Named("adapters.postgres.config").Error(fmt.Sprintf("Test %s", customHTTPApp))
+                caddy.Log().Named("adapters.postgres.config").Error(fmt.Sprintf("Testing %s", customHTTPApp))
                 config.AppsRaw["http"] = newHTTPAppConfig
             }
 		}
